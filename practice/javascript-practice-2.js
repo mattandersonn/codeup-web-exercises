c7392d7d14bf5a3b4be5c33bccc3a62f056cc970
Donny Anderson <donny.matt.anderson@gmail.com>
2020-12-08 20:29:49 -0600

    Javascript practice No. 2

---
+++ practice/javascript-practice-2.js
@@ -0,0 +1,137 @@
+
+/* MYVAR is not the same as MyVar nor myvar. It is possible to have multiple distinct variables with the same name but different casing. It is strongly recommended that for the sake of clarity, you do not use this language feature.
+//
+// Best Practice
+// Write variable names in JavaScript in camelCase. In camelCase, multi-word variable names have the first word in lowercase and the first letter of each subsequent word is capitalized.
+*/
+
+
+// Variable declarations
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+// Variable assignments
+STUDLYCAPVAR = 10;
+PRoperCAmelCAse = "A String";
+tITLEcASEoVER = 9000;
+
+
+    /* You can easily increment or add one to a variable with the ++ operator.
+
+     i++; is the equivalent of i = i + 1;
+
+
+   Note: he entire line becomes i++;, eliminating the need for the equal sign.  Change the code to use the ++ operator on myVar. */
+
+++ myVar;
+
+
+
+    // You can easily decrement or decrease a variable by one with the -- operator.
+    //
+    // i--;
+    //
+    // is the equivalent of
+    //
+    // i = i - 1;
+    //
+    // Note
+    // The entire line becomes i--;, eliminating the need for the equal sign.
+    //
+    //
+
+-- my Var;
+
+/*
+    n programming, it is common to use assignments to modify the contents of a variable. Remember that everything to the right of the equals sign is evaluated first, so we can say:
+
+    myVar = myVar + 5;
+
+    to add 5 to myVar. Since this is such a common pattern, there are operators which do both a mathematical operation and assignment in one step.
+
+    One such operator is the += operator. */
+
+    // a should equal 15.
+    //
+    // b should equal 26.
+    //
+    // c should equal 19.
+    //
+    // You should use the += operator for each variable.
+    //
+    // You should not modify the code above the specified comment.
+
+    var a = 3;
+    var b = 17;
+    var c = 12;
+
+    // Only change code below this line
+    a  += 12;
+    b  += 9;
+    c  += 7;
+
+   /* a should equal 15.
+
+    b should equal 26.
+
+    c should equal 19.
+
+    You should use the += operator for each variable.
+
+    You should not modify the code above the specified comment. */
+
+    // a should equal 5.
+    //
+    // Passed
+    // b should equal -6.
+    //
+    // Passed
+    // c should equal 2.
+    //
+    // Passed
+    // You should use the -= operator for each variable.
+    //
+    // Passed
+    // You should not modify the code above the specified comment.
+
+    var a = 11;
+    var b = 9;
+    var c = 3;
+
+    // Only change code below this line
+    a -= 6;
+    b -= 15;
+    c -= 1;
+
+
+
+/*
+The *= operator multiplies a variable by a number.
+
+myVar = myVar * 5;
+
+will multiply myVar by 5. This can be rewritten as:
+
+myVar *= 5;
+ */
+Convert the assignments for a, b, and c to use the *= operator.
+
+   /* a should equal 25.
+
+b should equal 36.
+
+c should equal 46.
+
+You should use the *= operator for each variable.
+    */
+
+
+    var a = 5;
+    var b = 12;
+    var c = 4.6;
+
+// Only change code below this line
+    a *= 5;
+    b *= 3;
+    c *= 10;
